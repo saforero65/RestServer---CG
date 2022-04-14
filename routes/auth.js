37042309bf2de89a31356ba8c135b1f592dd1d44
Santiago <saforero88@gmail.com>
2022-04-14 11:48:17 -0500

    google

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login, googleSignIn } = require("../controllers/auth");
-const { validarCampos } = require("../middlewares/validar-campos");
+const { login, googleSignIn, renovarToken } = require("../controllers/auth");
+const { validarCampos, validarJWT } = require("../middlewares");
 
 const router = Router();
 
@@ -23,5 +23,5 @@ router.post(
   ],
   googleSignIn
 );
-
+router.get("/", validarJWT, renovarToken);
 module.exports = router;
